d51c041640c4ad3bbf4aeeadb58668f4032d0d90
Anouk Van Bogaert <vb.anouk@gmail.com>
2022-09-27 00:54:30 +0000

    index.js for gathering all tests together

---
+++ lotide/index.js
@@ -0,0 +1,15 @@
+const head = require('./head');
+const tail = require('./tail');
+const middle = require('./middle');
+const eqArrays = require('./eqArrays');
+const assertEqual = require('./assertEqual');
+const assertArrayEqual = require('./assertArrayEqual');
+
+module.exports = {
+  head: head,
+  tail: tail,
+  middle: middle,
+  eqArrays: eqArrays,
+  assertEqual: assertEqual,
+  assertArrayEqual: assertArrayEqual
+};
