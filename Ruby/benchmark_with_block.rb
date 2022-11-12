61566bcc4bd92447bd66cd6b7c74da0b51a4741d
Anouk Van Bogaert <vb.anouk@gmail.com>
2022-11-12 22:38:54 +0000

    added filters

---
+++ Ruby/benchmark_with_block.rb
@@ -0,0 +1,15 @@
+def benchmark(arg)
+  # Your benchmarking code goes here.
+  start_time = Time.now
+  arg
+  end_time = Time.now
+
+end
+
+# Be careful, pasting this into IRB will take a long time to print.
+# It's a loooong string. :)
+long_string = "apple"*100000000
+
+running_time = benchmark ({ long_string.reverse })
+
+puts "string.reverse took #{running_time} seconds to run"
